22c80d6736069eb04d2b9061f9bc33e4bf1db60c
catcherwong <catcher_hwq@outlook.com>
2023-01-01 22:07:58 +0800

    list sort sample

---
+++ ListSortSample/ListSortSample/Program.cs
@@ -0,0 +1,39 @@
+﻿var list = new List<string>
+{ 
+    "阿莫西林", "阿司匹林", "阿卡波糖"
+};
+
+Console.WriteLine("Sort by default culture , {0}", Thread.CurrentThread.CurrentCulture.Name);
+list.Sort();
+
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("");
+
+Console.WriteLine("Sort by culture , en-US");
+list.Sort(StringComparer.Create(new System.Globalization.CultureInfo("en-US"), true));
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine("");
+
+
+Console.WriteLine("Sort by culture , zh-Hans-CN");
+list.Sort(StringComparer.Create(new System.Globalization.CultureInfo("zh-Hans-CN"), true));
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine("");
+
+Console.WriteLine($"CurrentCulture Name = {System.Globalization.CultureInfo.CurrentCulture.Name}"); 
+
+var all = System.Globalization.CultureInfo.GetCultures(System.Globalization.CultureTypes.AllCultures);
+foreach (var item in all)
+{
+    Console.WriteLine(item.Name);
+}
